82f3e90bd0848b3453b8dac1d9eb879573f52567
Omri Itzhaki <115615779+omriitzhaki2@users.noreply.github.com>
2024-03-31 16:40:13 +0300

    Update supermarket_map.py

--- pages/supermarket_map.py
+++ pages/supermarket_map.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
 from layout.app_layout import *
 from utils import *
 from PIL import Image
@@ -21,38 +22,28 @@ if st.session_state.user is None:
     st.error('You need to login or register')
     st.stop()
 
-# image = Image.open("layout/map.jpg")  # Replace "path_to_your_image.jpg" with the actual path to your image
-# img_bytes = io.BytesIO()
-# image.save(img_bytes, format="JPEG")
-# img_bytes.seek(0)
-#
-# if st.download_button(label="Click to download the map",
-#                       data=img_bytes,
-#                       file_name="SuperMarket Map.jpg",
-#                       mime="image/jpeg"):
-#     st.markdown(set_text('The map has been downloaded, enjoy the shopping!'), unsafe_allow_html=True)
-#     st.stop()
 
 st.markdown(set_subsubtitle(f'Your Fast Supermarket Route to {st.session_state.supermarket}'), unsafe_allow_html=True)
 products_list = st.session_state.shopping_list
 if not st.session_state.finish:
-    col1, col2 = st.columns([3, 1])
-    with col1:
-        G = create_distance_products_graph(products_list)
-        shortest_path = find_shortest_products_path_greedy(G)
-        plot_shortest_path_on_map(shortest_path)
-        st.image("layout/map.jpg")
-    with col2:
-        st.markdown("### Grocery List:")
-        for idx, (item, location) in enumerate(shortest_path, start=0):
-            if item != 'start' and item != 'end':
-                st.markdown(set_text(f"{idx}: {item}"), unsafe_allow_html=True)
-
+    G = create_distance_products_graph(products_list)
+    st.session_state.shortest_path = find_shortest_products_path_greedy(G)
+    plot_shortest_path_on_map(st.session_state.shortest_path)
     st.session_state.finish = True
     st.session_state.shopping_list = []
 
+col1, col2 = st.columns([3, 1])
+with col1:
+    st.image("layout/map.jpg")
+with col2:
+    st.markdown("### Grocery List:")
+    for idx, (item, location) in enumerate(st.session_state.shortest_path, start=0):
+        if item != 'start' and item != 'end':
+            st.markdown(set_text(f"{idx}: {item}"), unsafe_allow_html=True)
+
+
 # Save the image to a file
-image = Image.open("layout/map.jpg")  # Replace "path_to_your_image.jpg" with the actual path to your image
+image = Image.open("layout/map.jpg")
 img_bytes = io.BytesIO()
 image.save(img_bytes, format="JPEG")
 img_bytes.seek(0)
@@ -63,4 +54,6 @@ if st.download_button(label="Click to download the map",
                       mime="image/jpeg",
                       key='download_image'):
     st.markdown(set_text('The map has been downloaded, enjoy the shopping!'), unsafe_allow_html=True)
-    st.stop()
+
+if st.button('Return to home'):
+    switch_page('home')
